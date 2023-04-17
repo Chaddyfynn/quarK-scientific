1ea0a682ad6840e881b3fd334cd2a5a025a9ee7f
Chaddyfynn <chaddyfynn@gmail.com>
2023-04-17 22:22:28 +0100

    Fixed typedef definition

--- quarK/Vector.cpp
+++ quarK/Vector.cpp
@@ -2,38 +2,3 @@
 #include "Vector.h"
 #include <vector>
 #include <string>
-
-template <class T, int N>
-NVector<T, N>::NVector() {
-	 // Constructor contents
-}
-template <class T, int N>
-NVector<T, N>::~NVector() {
-	// Destructor contents
-}
-template <class T, int N>
-T NVector<T, N>::get(int num, ...) {
-	va_list valist;
-	va_start(valist, num);
-	NSVector<num, T> access = this->contents[va_arg(valist, int)];
-	for (int i = 0; i < num - 2; i++) {
-		NSVector<num - i, T> access = access[va_arg(valist, int)];
-	}
-	T element = access[va_arg(valist, int)];
-	return element;
-}
-template <class T, int N>
-void NVector<T, N>::set(T val, int num, ...) {
-}
-template <class T, int N>
-void NVector<T, N>::insert() {
-
-}
-template <class T, int N>
-void NVector<T, N>::printc() {
-
-}
-template <class T, int N>
-std::string NVector<T, N>::cat() {
-
-}
--- quarK/Vector.h
+++ quarK/Vector.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <iostream>
+#include <type_traits>
 
 #ifdef QUARK_EXPORTS
 #define QUARK_API __declspec(dllexport)
@@ -8,29 +10,70 @@
 #define QUARK_API __declspec(dllimport)
 #endif
 
+template<class T>
+bool is_vector(const std::vector<T>& x) { return true; }
+
+template<class T>
+bool is_vector(const T& x) { return false; }
+
+
+// NDVector (Tensor) for N>0
 template<size_t dimcount, typename T>
-struct NSVector
-{
-    typedef std::vector< typename NSVector<dimcount - 1, T>::type > type; // Recursive vector
-};
-
-template<typename T>
-struct NSVector<0, T>
-{
-    typedef T type; // 0 Dimensional vector end case
-};
-
-template<class T, int N>
-class QUARK_API NVector {
+class QUARK_API NDVector {
 public:
-    NSVector<N, T> contents; // Object vector data contents of N dimensions and type T
-    NVector(); // Constructor
-    ~NVector(); // Destructor
-    T get(int num, ...); // Get element at given position
-    void set(T val, int num, ...); // Set element at given position
+    // Internal DType Definition
+    typedef std::vector< typename NDVector<dimcount - 1, T>::ntype > ntype;  // Nested std::vector
+
+    // Constructors and Destructors
+    NDVector() {}; // Default constructor
+    NDVector(ntype init) : contents{ init } {}; // Initialiser constructor
+    ~NDVector(); // Destructor
+
+    // Getters and Setters
+    T getElement(std::vector<int> loc); // Return pointer to element (value, position)
+    ntype getContents() {
+        return this->contents;
+    }; // Return pointer to contents
+    void setElement(T val, std::vector<int>); // Set element value (value, position)
+    void setContents(ntype val); // Change the entire contents of the NDVector to arg
+
+    // Modify by Trivial Methods
+
+    // Modify by Addition or Subtraction
     void insert(); // Insert parameter1 to position parameter2 in the vector
-    void printc(); // Print the vector to the console 
-    std::string cat(); // Convert the vector to string
-protected:
+
+    // Modify by Scalar Multiplication or Division
+
+    // Vector Multiplication
+
+    // Modify by Log, Exp, Power, Root
+
+    // Slicing, Splicing and Dicing
+
+    // Conversions
+
+    // Representations
+    void print() { // Print the vector to the console 
+        
+    } 
+    std::string to_string(); // Return string representation of the contents
+
+    // Re-worded Methods (Addition -> Translation, Vector Product -> Rotation)
+
+    // Statistical Methods (Sum, Mean, Standard Deviation)
+
+    // Sampling Methods (Uniform, Normal)
+
+    // Import, Export
+
+    // Logic
 private:
+    ntype contents{}; // Vector contents
+};
+
+// Terminating (0D) Case
+template<typename T>
+class QUARK_API NDVector<0, T> {
+public:
+    typedef T ntype; 
 };
